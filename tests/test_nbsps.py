3449ac193a56e243ff0b852994028f9b9e223dbe
dhrions <aturing@laposte.net>
2025-08-27 19:02:52 +0200

    ✅ Add tests

--- tests/test_nbsps.py
+++ tests/test_nbsps.py
@@ -2,12 +2,14 @@
 import pytest
 from format_anki_notes.rules.nbsps import remove_all_nbsp, add_nbsp
 
+
 def test_remove_all_nbsp():
     """Teste la suppression de toutes les occurrences de &nbsp; dans le texte."""
     # Cas de base
     assert remove_all_nbsp("Hello&nbsp;world!") == "Hello world!"
     # Plusieurs &nbsp;
-    assert remove_all_nbsp("No&nbsp;&nbsp;&nbsp;nbsp&nbsp;here") == "No   nbsp here"
+    assert remove_all_nbsp(
+        "No&nbsp;&nbsp;&nbsp;nbsp&nbsp;here") == "No   nbsp here"
     # Aucun &nbsp;
     assert remove_all_nbsp("Already normal") == "Already normal"
     # Texte vide
@@ -16,6 +18,7 @@ def test_remove_all_nbsp():
     assert remove_all_nbsp("&nbsp;Hello&nbsp;") == " Hello "
     # Mélange avec des espaces normaux
     assert remove_all_nbsp("Hello&nbsp; world&nbsp;!") == "Hello  world !"
+
 
 def test_add_nbsp():
     """Teste l'ajout d'espaces insécables selon les règles typographiques françaises."""
@@ -34,6 +37,8 @@ def test_add_nbsp():
     # Espace insécable après ± et =
     assert add_nbsp('± 10') == '±&nbsp;10'
     assert add_nbsp('= 5') == '=&nbsp;5'
+    # assert add_nbsp('le signe = sert beaucoup') == 'le signe = sert beaucoup'
+    # assert add_nbsp('x = 5') == 'x&nbsp;=&nbsp;5'
     # Espace insécable pour mots-clés suivis d'un chiffre
     assert add_nbsp('article 4') == 'article&nbsp;4'
     assert add_nbsp('Article 58') == 'Article&nbsp;58'
@@ -57,7 +62,8 @@ def test_add_nbsp():
     # Espace insécable avant %
     assert add_nbsp('100 %') == '100&nbsp;%'
     # Cas avec plusieurs règles combinées
-    assert add_nbsp('« Test » : 2h30 et 10% ?') == '«&nbsp;Test&nbsp;»&nbsp;: 2&nbsp;h30 et 10&nbsp;%&nbsp;?'
+    assert add_nbsp(
+        '« Test » : 2h30 et 10% ?') == '«&nbsp;Test&nbsp;»&nbsp;: 2&nbsp;h30 et 10&nbsp;%&nbsp;?'
     # Texte sans modification nécessaire
     assert add_nbsp('Hello world') == 'Hello world'
     # Texte vide
@@ -65,11 +71,15 @@ def test_add_nbsp():
     # Cas avec HTML (à tester après intégration avec format_html)
     assert add_nbsp('Voir <b>article 4</b>') == 'Voir <b>article&nbsp;4</b>'
     # Cas avec guillemets et ponctuation
-    assert add_nbsp('« Test » : 2h30 et 10% ?') == '«&nbsp;Test&nbsp;»&nbsp;: 2&nbsp;h30 et 10&nbsp;%&nbsp;?'
+    assert add_nbsp(
+        '« Test » : 2h30 et 10% ?') == '«&nbsp;Test&nbsp;»&nbsp;: 2&nbsp;h30 et 10&nbsp;%&nbsp;?'
     # Date
     assert add_nbsp('24 mai') == '24 mai'
 
+
 def test_add_nbsp_with_anki_cloze():
-    assert add_nbsp('n<sup>o&nbsp;</sup>{{c1::52}}') == 'n<sup>o</sup>&nbsp;{{c1::52}}'
-    assert add_nbsp('n<sup>o</sup>{{c1::52}}') == 'n<sup>o</sup>&nbsp;{{c1::52}}'
+    assert add_nbsp(
+        'n<sup>o&nbsp;</sup>{{c1::52}}') == 'n<sup>o</sup>&nbsp;{{c1::52}}'
+    assert add_nbsp(
+        'n<sup>o</sup>{{c1::52}}') == 'n<sup>o</sup>&nbsp;{{c1::52}}'
     assert add_nbsp('n<sup>o</sup>52') == 'n<sup>o</sup>&nbsp;52'
